503eb33d790944988c7860b96d5eb88bcb442817
anirban-pal-web <anirban1481999@gmail.com>
2026-02-25 14:52:56 +0530

    Update userdata-primary.sh

--- terraform-influxdb-modular/userdata-primary.sh
+++ terraform-influxdb-modular/userdata-primary.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -e
+exec > /var/log/user-data.log 2>&1
+
+# FORCE APT TO USE IPV4
+echo 'Acquire::ForceIPv4 "true";' > /etc/apt/apt.conf.d/99force-ipv4
 
 ORG="devops-org"
 BUCKET="system-metrics"
 RET="30d"
 
 apt update -y
-apt install -y curl jq
+apt install -y curl jq gnupg lsb-release
 
-curl -s https://repos.influxdata.com/influxdata-archive.key | gpg --dearmor -o /usr/share/keyrings/influxdb.gpg
-echo "deb [signed-by=/usr/share/keyrings/influxdb.gpg] https://repos.influxdata.com/ubuntu jammy stable" > /etc/apt/sources.list.d/influxdb.list
+curl -fsSL https://repos.influxdata.com/influxdata-archive.key | \
+  gpg --dearmor -o /usr/share/keyrings/influxdb.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/influxdb.gpg] \
+https://repos.influxdata.com/ubuntu jammy stable" \
+> /etc/apt/sources.list.d/influxdb.list
 
 apt update -y
 apt install -y influxdb2
@@ -17,9 +25,7 @@ apt install -y influxdb2
 systemctl enable influxdb
 systemctl start influxdb
 
-# Wait until InfluxDB is healthy
 until curl -s http://localhost:8086/health | grep -q "pass"; do
-  echo "Waiting for InfluxDB..."
   sleep 5
 done
 
