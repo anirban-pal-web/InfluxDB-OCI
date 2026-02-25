081f105d1111602843427f6932fc49b5e7d07df1
anirban-pal-web <anirban1481999@gmail.com>
2026-02-25 17:28:13 +0530

    Update main.tf

--- terraform-influxdb-modular/modules/vpc-peering/main.tf
+++ terraform-influxdb-modular/modules/vpc-peering/main.tf
@@ -8,27 +8,23 @@ resource "aws_vpc_peering_connection" "this" {
   }
 }
 
-# Routes in requester VPC
+# Route in requester PRIVATE route table
 resource "aws_route" "requester_private_route" {
   route_table_id            = var.requester_route_table_ids[0]
   destination_cidr_block    = var.peer_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
 
+# Route in requester PUBLIC route table
 resource "aws_route" "requester_public_route" {
   route_table_id            = var.requester_route_table_ids[1]
   destination_cidr_block    = var.peer_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
 
-# Routes in peer VPC
+# Route in peer VPC
 resource "aws_route" "peer_route" {
   route_table_id            = var.peer_route_table_ids[0]
-  destination_cidr_block    = var.requester_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.this.id
-}
-
-  route_table_id            = each.value
   destination_cidr_block    = var.requester_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
